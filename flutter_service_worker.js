f85e92e38cad9ea1a04e478089035f10583a7d53
vidhi26apr <vidhijain26apr@gmail.com>
2025-06-05 13:24:15 +0530

    Built web
    Branch: main
    Commit: e796b46c83a7c6ff89cf4a344d2fec6a95dc1c36
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "c3502eaa895829cc922d3598d15b87ea",
+"flutter_bootstrap.js": "3d0fb9fbe05bbb119f19b5ad6ebacbfb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
